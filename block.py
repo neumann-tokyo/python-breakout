dbd9600d4905bf87f15e5f8152037ce274651315
kbaba1001 <kbaba1001@users.noreply.github.com>
2022-07-28 18:49:09 +0900

    show bar

--- block.py
+++ block.py
@@ -2,9 +2,11 @@ import pygame as pg, sys
 import random
 pg.init()
 screen = pg.display.set_mode((800, 600))
+barrect = pg.Rect(400, 500, 100, 20)
 
 def gamestage():
     screen.fill(pg.Color("NAVY"))
+    pg.draw.rect(screen, pg.Color("CYAN"), barrect)
 
 while True:
     gamestage()
