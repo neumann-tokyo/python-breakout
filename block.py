fd706758ffe3efb1c13039f77567936b6dfd12d4
kbaba1001 <kbaba1001@users.noreply.github.com>
2022-07-29 18:29:02 +0900

    feat: gameover

--- block.py
+++ block.py
@@ -9,9 +9,11 @@ blocks = []
 for yy in range(4):
     for xx in range(7):
         blocks.append(pg.Rect(50+xx*100, 40+yy*50, 80, 30))
+page = "play"
 
 def gamestage():
     global vx, vy
+    global page
     screen.fill(pg.Color("NAVY"))
     (mx, my) = pg.mouse.get_pos()
     barrect.x = mx - 50
@@ -22,6 +24,8 @@ def gamestage():
         vx = -vx
     if barrect.colliderect(ballrect):
         vy = -vy
+    if ballrect.y > 600:
+        page = "gameover"
     ballrect.x += vx
     ballrect.y += vy
     pg.draw.circle(screen, pg.Color("CYAN"), [ballrect.x, ballrect.y], ballrect.width)
@@ -33,8 +37,17 @@ def gamestage():
             blocks[n] = pg.Rect(0,0,0,0)
         n += 1
 
+def gameover():
+    screen.fill(pg.Color("NAVY"))
+    font = pg.font.Font(None, 150)
+    text = font.render("GAMEOVER", True, pg.Color("RED"))
+    screen.blit(text, (100, 200))
+
 while True:
-    gamestage()
+    if page == "play":
+        gamestage()
+    elif page == "gameover":
+        gameover()
     pg.display.update()
     pg.time.Clock().tick(60)
     for event in pg.event.get():
