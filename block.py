9f0d235ab66b309bde761e1e4eb2232f4d682820
kbaba1001 <kbaba1001@users.noreply.github.com>
2022-07-29 18:07:38 +0900

    feat: show blocks

--- block.py
+++ block.py
@@ -4,6 +4,10 @@ pg.init()
 screen = pg.display.set_mode((800, 600))
 barrect = pg.Rect(400, 500, 100, 20)
 ballrect = pg.Rect(400, 450, 10, 10)
+blocks = []
+for yy in range(4):
+    for xx in range(7):
+        blocks.append(pg.Rect(50+xx*100, 40+yy*50, 80, 30))
 
 def gamestage():
     screen.fill(pg.Color("NAVY"))
@@ -11,6 +15,8 @@ def gamestage():
     barrect.x = mx - 50
     pg.draw.rect(screen, pg.Color("CYAN"), barrect)
     pg.draw.circle(screen, pg.Color("CYAN"), [ballrect.x, ballrect.y], ballrect.width)
+    for block in blocks:
+        pg.draw.rect(screen, pg.Color("GOLD"), block)
 
 while True:
     gamestage()
