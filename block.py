2bc61d8dd2cd16a8b94d78ce7ace633b79a472a0
kbaba1001 <kbaba1001@users.noreply.github.com>
2022-07-29 18:05:12 +0900

    feat: show ball

--- block.py
+++ block.py
@@ -3,12 +3,14 @@ import random
 pg.init()
 screen = pg.display.set_mode((800, 600))
 barrect = pg.Rect(400, 500, 100, 20)
+ballrect = pg.Rect(400, 450, 10, 10)
 
 def gamestage():
     screen.fill(pg.Color("NAVY"))
     (mx, my) = pg.mouse.get_pos()
     barrect.x = mx - 50
     pg.draw.rect(screen, pg.Color("CYAN"), barrect)
+    pg.draw.circle(screen, pg.Color("CYAN"), [ballrect.x, ballrect.y], ballrect.width)
 
 while True:
     gamestage()
